271c4d97f0deded709f070fc236c2c518244005d
Rafael Pereira <rpsouza.dev@gmail.com>
2023-07-20 11:02:38 -0300

    Created RecyclerView structure

--- app/src/main/java/com/example/guests/view/AllGuestsFragment.kt
+++ app/src/main/java/com/example/guests/view/AllGuestsFragment.kt
@@ -7,37 +7,44 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.guests.databinding.FragmentAllGuestsBinding
+import com.example.guests.view.adapter.GuestsAdapter
 import com.example.guests.viewmodel.AllGuestsViewModel
 
 class AllGuestsFragment : Fragment() {
 
     private var _binding: FragmentAllGuestsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var allGuestsViewModel: AllGuestsViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val allGuestsViewModel =
-            ViewModelProvider(this).get(AllGuestsViewModel::class.java)
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, b: Bundle?): View {
+        allGuestsViewModel = ViewModelProvider(this).get(AllGuestsViewModel::class.java)
         _binding = FragmentAllGuestsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        allGuestsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        // Layout
+        binding.recycleAllGuests.layoutManager = LinearLayoutManager(context)
+
+        // Adapter
+        binding.recycleAllGuests.adapter = GuestsAdapter()
+
+        allGuestsViewModel.getAll()
+
+        observe()
+
+        return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun observe() {
+        allGuestsViewModel.guestsAll.observe(viewLifecycleOwner) {
+            val s = ""
+
+        }
     }
 }
--- app/src/main/java/com/example/guests/viewmodel/AllGuestsViewModel.kt
+++ app/src/main/java/com/example/guests/viewmodel/AllGuestsViewModel.kt
@@ -1,13 +1,19 @@
 package com.example.guests.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import com.example.guests.model.GuestModel
+import com.example.guests.repository.GuestRepository
 
-class AllGuestsViewModel : ViewModel() {
+class AllGuestsViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository = GuestRepository.getInstance(application.applicationContext)
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val listAllGuests = MutableLiveData<List<GuestModel>>()
+    val guestsAll: LiveData<List<GuestModel>> = listAllGuests
+
+    fun getAll() {
+       listAllGuests.value = repository.getAll()
     }
-    val text: LiveData<String> = _text
 }
