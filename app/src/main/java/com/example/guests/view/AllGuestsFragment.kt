0c074af7de83b0c5af4ab0feb6db7e60752024b2
Rafael Souza <rpsouza.dev@gmail.com>
2023-07-22 12:44:46 -0300

    Add application event structure

--- app/src/main/java/com/example/guests/view/AllGuestsFragment.kt
+++ app/src/main/java/com/example/guests/view/AllGuestsFragment.kt
@@ -5,12 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.guests.databinding.FragmentAllGuestsBinding
 import com.example.guests.view.adapter.GuestsAdapter
+import com.example.guests.view.listener.OnGuestListener
 import com.example.guests.viewmodel.AllGuestsViewModel
 
 class AllGuestsFragment : Fragment() {
@@ -29,6 +31,19 @@ class AllGuestsFragment : Fragment() {
 
         // Adapter
         binding.recycleAllGuests.adapter = adapter
+
+        val listener = object : OnGuestListener {
+            override fun onClick(id: Int) {
+                Toast.makeText(context, "Olá, fui clicado!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onDelete(id: Int) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        adapter.attachListener(listener)
 
         allGuestsViewModel.getAll()
 
--- app/src/main/java/com/example/guests/view/viewholder/GuestViewHolder.kt
+++ app/src/main/java/com/example/guests/view/viewholder/GuestViewHolder.kt
@@ -6,9 +6,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.guests.R
 import com.example.guests.databinding.RowGuestBinding
 import com.example.guests.model.GuestModel
+import com.example.guests.view.listener.OnGuestListener
 
-class GuestViewHolder(private val bind: RowGuestBinding) : RecyclerView.ViewHolder(bind.root) {
+class GuestViewHolder(private val bind: RowGuestBinding, private val listener: OnGuestListener) :
+    RecyclerView.ViewHolder(bind.root) {
     fun bind(guest: GuestModel) {
         bind.textName.text = guest.name
+
+        bind.textName.setOnClickListener {
+            listener.onClick(guest.id)
+        }
     }
 }
